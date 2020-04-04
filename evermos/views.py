dbc5867f10267383f08251b4c5f689ed229db0ab
annazarryuu <annazar.ryuu@gmail.com>
2020-04-04 09:18:05 +0000

    nothing

--- evermos/views.py
+++ evermos/views.py
@@ -41,7 +41,7 @@ class requestToAPI() :
         if path != None :
             files = {'document': open(os.path.join(BASE_DIR, 'media/' + path),'rb')}
             print('dir : ', os.path.join(BASE_DIR, 'media'))
-            # r = requests.post(self.API_URL, files=files, headers=self.API_HEADER)
+            r = requests.post(self.API_URL, files=files, headers=self.API_HEADER)
             additional['images'] = ['img/products/1525.jpg']
             additional['onsearch'] = '/media/' + path
         return {**self.context, **additional}
@@ -55,7 +55,7 @@ class requestToAPI() :
             return redirect('/image-search/' + path)
         except :
             return redirect('/image-search')
-        
+
 class home(TemplateView) :
 
     def get(self, request):
